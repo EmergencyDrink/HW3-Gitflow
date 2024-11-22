5bba71e7397c2313c98ca07a947de90e1ea0c7de
EmergencyDrink <168494653+EmergencyDrink@users.noreply.github.com>
2024-11-22 15:57:55 -0500

    First commit
    Funcion para agregar

---
+++ app.js
@@ -0,0 +1,26 @@
+const form = document.getElementById('alumnoForm');
+const tabla = document.getElementById('TablaAlumnos').querySelector('tbody')
+let filaEnEdicion = null;
+const btnAgregar = document.getElementById('btnAgregar');
+
+form.addEventListener('submit', (event)=>{
+    event.preventDefault();
+
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const matricula = document.getElementById('matricula').value;
+    const nota = document.getElementById('nota').value;
+        
+    //Agregar
+    const fila = document.createElement('tr');
+    fila.innerHTML = `
+            <td>${nombre}</td>
+            <td>${apellido}</td>
+            <td>${matricula}</td>
+            <td>${nota}</td>
+            <td><button class = "Delete-btn btn-danger">Delete</button>
+            <button class = "Editar-btn btn-primary">Editar</button></td>`;
+        tabla.appendChild(fila)
+    
+    form.reset(); 
+});
